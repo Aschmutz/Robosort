b6118cc799888b6f1781275eaf473016e8ae968d
Leah Vanessa Schmutz <LeahVanessaSchmutz@gmail.com>
2024-02-27 18:07:04 +0100

    FIxed the project to work correctly in the URP (SSAO still missing)

--- Assets/Scripts/ControllableOBJLoader.cs
+++ Assets/Scripts/ControllableOBJLoader.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class ControllableOBJLoader : IModelLoader
 {
+    [SerializeField] private Material material;
     public override GameObject Load(string path)
     {
-        GameObject loading = new Dummiesman.OBJLoader().Load(path, path[0..^3] + ".mtl");
+        GameObject loading = new Dummiesman.OBJLoader().Load(path);
         GameObject loadingChild = loading.transform.GetChild(0).gameObject;
         MeshFilter mf = loadingChild.GetComponent<MeshFilter>();
         MeshCollider mc = loadingChild.AddComponent<MeshCollider>();
         mc.convex = true;
         mc.sharedMesh = mf.mesh;
         Rigidbody rb = loadingChild.AddComponent<Rigidbody>();
+        MeshRenderer mr = loadingChild.GetComponent<MeshRenderer>();
+        mr.material = material;
         GameObject loaded = Instantiate(loadingChild, Vector3.zero, Quaternion.identity);
         DestroyImmediate(loading);
         return loaded;
