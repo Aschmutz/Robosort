2280bcea1dd0c247feb01bc83b4fa6a149fa3f95
Leah Vanessa Schmutz <LeahVanessaSchmutz@gmail.com>
2024-02-28 16:18:35 +0100

    Finalized the URP switch and implemented SSAO

--- Assets/Scripts/Rendering.cs
+++ Assets/Scripts/Rendering.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
@@ -31,27 +32,22 @@ public class Rendering : MonoBehaviour
     public IEnumerator RenderSingleImage(string path, float simulationTime, string prefix)
     {
         //simulation delay
-        if(simulationTime > 0)
+        if (simulationTime > 0)
             yield return new WaitForSeconds(simulationTime);
         cameraLeft.enabled = true;
         cameraRight.enabled = true;
-        //Lighting Delay
-        //yield return null;
-        
         RenderTexture.active = renderTexture;
         cameraLeft.Render();
         cameraRight.Render();
+
         Texture2D t2d = new Texture2D(renderTexture.width, renderTexture.height,textureFormat,true);
         t2d.ReadPixels(new Rect(0,0,renderTexture.width, renderTexture.height),0,0);
         t2d.Apply();
         byte[] pngPixels = t2d.EncodeToPNG();
         
-        SaveRenderToFile(pngPixels,path, prefix,m_ImageRenderId++); 
-        
+        SaveRenderToFile(pngPixels,path, prefix,m_ImageRenderId++);
         cameraLeft.enabled = false;
         cameraRight.enabled = false;
-        //yield return null;
-        //m_RunningRender = null; // yield return null must be above this line, otherwise the value will not be set to null for some reason. another fix would be to wait for a longer amount of time 
     }
 
     private async void SaveRenderToFile (byte[] imageData, string path, string prefix,int id)
